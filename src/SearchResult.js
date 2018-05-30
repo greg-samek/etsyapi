e0127311bb7440978e0e05937fffc78653d51e25
greg-samek <gas222@cornell.edu>
2018-05-30 13:35:10 -0400

    Some files un-staged.

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -3,19 +3,43 @@ import './SearchResult.css';
 import Header from './Header';
 
 
-class Home extends Component {
+class SearchResult extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {data: null};
+    }
+
+    componentDidMount() {
+        const { match: { params } } = this.props;
+        //TODO: because of CORS limitations, I'm making a request to a simple node proxy I created.
+        const that = this;
+        fetch(`http://localhost:4000/api?keywords=${params.SearchTerms}`)
+            .then(
+                (response) => {
+                    response.json().then(function(data) {
+                        that.setState({data: data});
+                    });
+                }
+            )
+            .catch(
+                (error) => {
+                    console.log(error);
+                }
+            )
+    }
 
     render() {
         const { match: { params } } = this.props;
         return(
             <div>
                 <Header/>
-                <body>
+                <div>
                 <p className="empty-search">You searched for {params.SearchTerms}</p>
-                </body>
+                </div>
             </div>
         )
     }
 }
 
-export default Home;
+export default SearchResult;
